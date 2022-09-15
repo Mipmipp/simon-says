0f1878259c7efcb1f5d93fdc8b214121c646619f
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-09-15 17:53:59 -0300

    Create functions to highlight boxes and return it

--- game.js
+++ game.js
@@ -1,3 +1,11 @@
 let machinePattern = []; 
 let userPattern = [];
 
+
+function highlightBox(randomBox) {
+    document.querySelector(`${randomBox}`).className = 'highlight';
+}
+
+function returnColorBox(randomBox) {
+    document.querySelector(`${randomBox}`).className = 'return-color';
+}
