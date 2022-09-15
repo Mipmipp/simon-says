51d1a58b87d14da668b4a10b9181763ed7774fef
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-09-15 17:54:21 -0300

    Create function to get a random box

--- game.js
+++ game.js
@@ -2,6 +2,24 @@ let machinePattern = [];
 let userPattern = [];
 
 
+function getRandomBox() {
+    const totalBoxes = 4;
+    let randomBoxNumber = Math.floor(Math.random() * totalBoxes) + 1;
+    
+    if(randomBoxNumber === 1) {
+        return '#red-box';
+    }
+    if(randomBoxNumber === 2) {
+        return '#blue-box';
+    }
+    if(randomBoxNumber === 3) {
+        return '#green-box';
+    }
+    if(randomBoxNumber === 4) {
+        return '#yellow-box';
+    }
+}
+
 function highlightBox(randomBox) {
     document.querySelector(`${randomBox}`).className = 'highlight';
 }
