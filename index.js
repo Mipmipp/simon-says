5c26b89099f09f8909295ec5ff4598877bea4f57
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-09-20 23:37:35 -0300

    Give functions to buttons to change the interface

--- index.js
+++ index.js
@@ -1,16 +1,59 @@
 const $main = document.querySelector('.main');
 
-document.querySelector('#buttons-difficult').onclick = function(event) {
+document.querySelector('#slow').onclick = function(event) {
     event.preventDefault();
+    milliSecondsDifficult = 2000;
     hideElement('#homepage');
     showElement('#game');
+    showElement('#start-game');
+    hideElement('#actual-state');
+}
+
+document.querySelector('#normal').onclick = function(event) {
+    event.preventDefault();
+    milliSecondsDifficult = 1000;
+    hideElement('#homepage');
+    showElement('#game');
+    showElement('#start-game');
+    hideElement('#actual-state');
+}
+
+document.querySelector('#fast').onclick = function(event) {
+    event.preventDefault();
+    milliSecondsDifficult = 500;
+    hideElement('#homepage');
+    showElement('#game');
+    showElement('#start-game');
+    hideElement('#actual-state');
 }
 
 document.querySelector('#start-game').onclick = function(event) {
     event.preventDefault();
     hideElement('#start-game');
-    showElement('#actual-score');
+    showElement('#actual-state');
+    machinePattern = [];
     handleGame();
+}
+
+document.querySelector('#try-again').onclick = function(event) {
+    event.preventDefault();
+    hideElement('#player-lose');
+    hideElement('#actual-state');
+    showElement('#start-game');
+    showElement('#game');
+    document.querySelector('#start-game').onclick = function(event) {
+        event.preventDefault();
+        hideElement('#start-game');
+        showElement('#actual-state');
+        machinePattern = [];
+        handleGame();
+    }
+}
+
+document.querySelector('#change-difficulty').onclick = function(event) {
+    event.preventDefault();
+    hideElement('#player-lose');
+    showElement('#homepage');
 }
 
 function showElement(element) {
